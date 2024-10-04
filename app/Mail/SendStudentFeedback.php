e4269e8ad1db22a21e666d0f5e254fdc478767c7
Owuraku <boatseth3@gmail.com>
2024-10-04 20:05:50 +0000

    added feedback email to the queue

--- app/Mail/SendStudentFeedback.php
+++ app/Mail/SendStudentFeedback.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendStudentFeedback extends Mailable
+class SendStudentFeedback extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
