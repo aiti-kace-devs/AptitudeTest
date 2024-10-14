8227ca9f445d159f39398801779a7bfdf3ca5f28
Owuraku <boatseth3@gmail.com>
2024-10-14 11:06:51 +0000

    updated email

--- resources/views/mail/generic-email.blade.php
+++ resources/views/mail/generic-email.blade.php
@@ -5,9 +5,20 @@ Kindly join using the new link below
 
 <a href="{{$data['link']}}">{{$data['link']}}</a>
 
+or click the button below
 <x-mail::button :url="$data['link']">
 Join Meeting
 </x-mail::button>
+
+@if ($data['whatsapp'])
+Please join the WhatsApp Group for {{$data['course']}} by clicking the button below
+<x-mail::button :url="$data['whatsapp']" color="success">
+Join WhatsApp
+</x-mail::button>
+
+If button is not working copy this: <a href="{{$data['whatsapp']}}">{{$data['whatsapp']}}</a>
+
+@endif
 
 This link supersedes any other link shared previously.
 
