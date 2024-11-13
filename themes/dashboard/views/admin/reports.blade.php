fdbdf357d82b1a2f4f3f41db2decc0344c331024
Owuraku <boatseth3@gmail.com>
2024-11-13 22:18:49 +0000

    WIP, added view

---
+++ themes/dashboard/views/admin/reports.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app')
+@section('title', 'View Attendance')
+@section('content')
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <!-- /.content-header -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">View Attendance Report</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">View Attendance Report</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <!-- Default box -->
+                            <div class="row">
+                                <div class="mb-4 col-md-4">
+                                    <label for="course_id" class="form-label">Select Course</label>
+                                    <select name="course_id" id="course_id" class="form-control">
+                                        <option value="">Select Course</option>
+
+                                        {{-- @foreach ($courses as $course)
+                                            <option value="{{ $course->id }}"
+                                                @if ($course->id == $selectedCourse?->id) selected @endif>
+                                                {{ $course->location }} -
+                                                {{ $course->course_name }}</option>
+                                        @endforeach --}}
+                                    </select>
+                                </div>
+                                <div class="mb-4 col-md-4">
+                                    <label for="date">Select Date</label>
+                                    <input type="text" name="dates" id="selected_date" class="form-control" required>
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <table class="table table-striped table-bordered table-hover datatable">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Student Name</th>
+                                            <th>Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {{-- @foreach ($attendance as $key => $record)
+                                            <tr>
+                                                <td>{{ $key + 1 }}</td>
+                                                <td>{{ $record->name }}</td>
+                                                <td>{{ $record->email }}</td>
+                                            </tr>
+                                        @endforeach --}}
+                                    </tbody>
+                                    <tfoot>
+
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+        </div>
+        </section>
+    </div>
+    <!-- /.content-header -->
+@endsection
+@push('scripts')
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('input[name="dates"]').daterangepicker({
+                showWeekNumbers: true,
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
+                        'month').endOf('month')]
+                }
+            });
+        });
+    </script>
+@endpush
