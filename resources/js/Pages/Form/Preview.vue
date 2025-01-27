8377bb828bbae3179797ece7e5647527fe73dbff
Owusu Ansah Gerald <geraldowusuansah2@gmail.com>
2025-01-27 17:43:14 +0000

    Updated Preview file

--- resources/js/Pages/Form/Preview.vue
+++ resources/js/Pages/Form/Preview.vue
@@ -6,7 +6,6 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
 import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
-import { ref } from "vue";
 import SelectInput from "@/Components/SelectInput.vue";
 import Checkbox from "@/Components/Checkbox.vue";
 import DangerButton from "@/Components/DangerButton.vue";
@@ -31,51 +30,32 @@ export default {
     admissionForm: Object,
   },
   data() {
-    const selections = ref([]);
-    const form = useForm({
+    const formFields = {
       form_uuid: this.admissionForm.uuid,
+      response_data: {},
+    };
+
+    this.admissionForm.schema.forEach((schema) => {
+      if (
+        ["text", "number", "email", "file", "password", "radio"].includes(schema.type)
+      ) {
+        formFields.response_data[schema.field_name] = null;
+      } else if (schema.type === "checkbox") {
+        formFields.response_data[schema.field_name] = [];
+      } else if (schema.type === "select") {
+        formFields.response_data[schema.field_name] = "";
+      }
     });
+
+    const form = useForm(formFields);
 
     return {
       form,
-      selections,
     };
-  },
-  mounted() {
-    // Initialize dynamic fields based on the schema provided
-    this.admissionForm.schema.forEach((schema, index) => {
-      const fieldName = schema.title.replace(/\s+/g, "_").toLowerCase();
-
-      if (!this.form.hasOwnProperty(fieldName)) {
-        if (
-          ["text", "number", "email", "file", "password", "radio"].includes(schema.type)
-        ) {
-          this.form[fieldName] = null;
-        } else if (["checkbox"].includes(schema.type)) {
-          this.form[fieldName] = [];
-        } else if (["select"].includes(schema.type)) {
-          this.form[fieldName] = "";
-        }
-      }
-
-      // Push the field details into selections
-      const newField = {
-        id: `field_${index + 1}`,
-        label: schema.title,
-        title: schema.title,
-        type: schema.type,
-        fieldName: fieldName,
-        placeholder: schema.title,
-        options: schema.options || [],
-        is_required: schema.is_required,
-      };
-
-      this.selections.push(newField);
-    });
   },
   methods: {
     submit() {
-      this.form.post(route("admission.store"), {
+      this.form.post(route("form_responses.store"), {
         onSuccess: () => {
           toastr.success("Entry successfully submitted");
           this.resetForm();
@@ -88,7 +68,6 @@ export default {
     resetForm() {
       this.form.reset();
       this.form.clearErrors();
-      this.selections = [];
     },
   },
 };
@@ -119,15 +98,13 @@ export default {
             <div class="mt-4">
               <form @submit.prevent="submit">
                 <div class="space-y-5">
-                  <div v-for="(question, index) in selections" :key="index">
+                  <div v-for="(question, index) in admissionForm.schema" :key="index">
                     <div>
                       <InputLabel
                         :for="`field-${index}`"
                         :value="question.title"
                         :required="question.is_required"
                       />
-
-                      <!-- Text Input -->
                       <TextInput
                         v-if="
                           ['text', 'number', 'email', 'password'].includes(question.type)
@@ -135,23 +112,23 @@ export default {
                         :id="`field-${index}`"
                         :type="question.type"
                         class="mt-1 w-full"
-                        v-model="form[question.fieldName]"
-                        :placeholder="question.placeholder"
-
+                        v-model="form.response_data[question.field_name]"
+                        :placeholder="question.title"
                         :class="{
                           'block w-full mt-2 text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-gray-50 file:text-gray-700 hover:file:bg-gray-100':
                             question.type == 'file',
                           'file:bg-red-600 hover:file:bg-red-500 file:text-white':
                             question.type == 'file',
-                          'border-red-600': form.errors[question.fieldName],
+                          'border-red-600':
+                            form.errors[`response_data.${question.field_name}`],
                         }"
                       />
 
                       <!-- Select Input -->
                       <div v-else-if="question.type === 'select'">
                         <SelectInput
-                          :id="question.fieldName"
-                          v-model="form[question.fieldName]"
+                          :id="question.field_name"
+                          v-model="form.response_data[question.field_name]"
                           class="mt-1 w-full"
                         >
                           <option value="" disabled selected>
@@ -167,15 +144,18 @@ export default {
                         </SelectInput>
                       </div>
 
-                      <!-- Checkbox Input -->
-                      <div class="flex items-center space-x-4" v-else-if="question.type === 'checkbox'">
-                        <div class="mt-1 flex items-center space-x-2"
+                      <div
+                        class="flex items-center space-x-4"
+                        v-else-if="question.type === 'checkbox'"
+                      >
+                        <div
+                          class="mt-1 flex items-center space-x-2"
                           v-for="(option, idx) in question.options.split(',')"
                           :key="idx"
                         >
                           <Checkbox
                             :id="`field-${index}-option-${idx}`"
-                            v-model:checked="form[question.fieldName]"
+                            v-model:checked="form.response_data[question.field_name]"
                             :value="option.trim()"
                           />
                           <InputLabel
@@ -185,8 +165,29 @@ export default {
                         </div>
                       </div>
 
-                      <!-- Show any error message -->
-                      <InputError :message="form.errors[question.fieldName]" />
+                      <div
+                        class="flex items-center gap-4"
+                        v-else-if="question.type == 'radio'"
+                      >
+                        <div
+                          class="flex items-center space-x-2"
+                          v-for="(option, idx) in question.options.split(',')"
+                          :key="idx"
+                        >
+                          <RadioInput
+                            :id="`field-${index}-option-${idx}`"
+                            v-model:checked="form.response_data[question.field_name]"
+                            :value="option.trim()"
+                          />
+                          <InputLabel
+                            :for="`field-${index}-option-${idx}`"
+                            :value="option.trim()"
+                          />
+                        </div>
+                      </div>
+                      <InputError
+                        :message="form.errors[`response_data.${question.field_name}`]"
+                      />
                     </div>
                   </div>
 
