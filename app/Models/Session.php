f9204300e3f1f04ba271f3caa589c39d10e6f720
Owusu Ansah Gerald <63270640+geraldsi7@users.noreply.github.com>
2025-04-02 00:11:42 +0000

    added session crud (#12)
    * added session crud
    
    * updated session crud
    
    * added exists validation
    
    ---------
    
    Co-authored-by: Owuraku <boatseth3@gmail.com>

---
+++ app/Models/Session.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Session extends Model
+{
+    use HasFactory;
+
+    protected $table = 'course_sessions';
+
+    protected $fillable = [
+        'name',
+        'course_id',
+        'limit',
+        'course_time',
+        'session'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = (string) Str::uuid();
+            }
+        });
+    }
+}
