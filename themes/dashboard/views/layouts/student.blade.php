1da78358ef2d83b528f2fe1cee106008c682dda8
konadu91 <dankwahjessica91@gmail.com>
2025-04-03 12:45:54 +0000

    added favicons

--- themes/dashboard/views/layouts/student.blade.php
+++ themes/dashboard/views/layouts/student.blade.php
@@ -10,6 +10,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> @yield('title')</title>
 
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets') }}/images/logo.png">
+    <link rel="icon" type="image/png" href="{{ asset('assets') }}/images/logo.png">
+
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
