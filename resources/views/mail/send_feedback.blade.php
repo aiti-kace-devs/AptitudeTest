52c9d54ef2384a000a569688506d495abd5326df
Owuraku <boatseth3@gmail.com>
2025-04-09 18:36:22 +0000

    changed email date

--- resources/views/mail/send_feedback.blade.php
+++ resources/views/mail/send_feedback.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 # Hi,
-We acknowledge your assessment test submission. Please note that shortlisted applicants will be contacted before 10th April, 2025.
+We acknowledge your assessment test submission. Please note that shortlisted applicants will be contacted before 13th April, 2025.
 
 @component('mail::panel')
     For more information, kindly send a whatsapp message on **0556660355** or call us on **0536466032**
