ebfb5a00c606e9f31766343f43c4fd65254465c2
Owuraku <boatseth3@gmail.com>
2025-04-12 15:24:37 +0000

    added foreign keys and indexes to optimize db queries

--- database/migrations/2025_04_10_160922_add_indexes_to_tables.php
+++ database/migrations/2025_04_10_160922_add_indexes_to_tables.php
@@ -11,65 +11,182 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::table('users', function (Blueprint $table) {
-        //     // column type changes
-        //     $table->unsignedBigInteger('exam')->change();
+        Schema::table('oex_exam_masters', function (Blueprint $table) {
+            $table->unsignedBigInteger('category')->change();
+            $table->unsignedBigInteger('id')->change();
 
-        //     // index
-        //     $table->index('exam');
-        //     // $table->unsignedBigInteger('exam')->change();
-        //     $table->index('age');
-        //     $table->index('userId');
-        // });
+            // index
+            $table->unique('title');
 
-        // Schema::table('user_admission', function (Blueprint $table) {
-        //     // column type changes
-        //     $table->unsignedBigInteger('course_id')->change();
-        //     $table->unsignedBigInteger('session')->change();
+            // foreign keys
+            $table->foreign('category')->references('id')->on('oex_categories')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
 
-        //     // index
-        //     $table->unique('user_id');
-        //     $table->index(['user_id', 'course_id']);
-        // });
+        Schema::table('courses', function (Blueprint $table) {
+            // index
+            $table->index('id');
+            $table->unique('course_name');
+            $table->unique([
+                'centre_id',
+                'programme_id'
+            ]);
 
-        // Schema::table('courses', function (Blueprint $table) {
-        //     // index
-        //     $table->index('centre_id');
-        //     $table->index('programme_id');
-        // });
+            // foreign keys
+            $table->foreign('centre_id')->references('id')->on('centres')
+                ->onDelete('restrict')->onUpdate('cascade');
 
-        // Schema::table('centres', function (Blueprint $table) {
-        //     // index
-        //     $table->index('branch_id');
-        // });
+            $table->foreign('programme_id')->references('id')->on('programmes')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
 
-        // Schema::table('user_exams', function (Blueprint $table) {
-        //     // column type changes
-        //     $table->unsignedBigInteger('user_id')->change();
-        //     $table->unsignedBigInteger('exam_id')->change();
+        Schema::table('users', function (Blueprint $table) {
+            // column type changes
+            $table->unsignedBigInteger('exam')->change();
 
-        //     // index
-        //     $table->index('user_id');
-        //     $table->index('exam_id');
-        //     $table->index(['exam_id', 'user_id']);
-        //     $table->index('exam_joined');
-        // });
+            // index
+            $table->index('id');
+            $table->unique('userId');
+
+
+            // foreign keys
+            $table->foreign('exam')->references('id')->on('oex_exam_masters')
+                ->onDelete('restrict')->onUpdate('cascade');
+
+            $table->foreign('registered_course')->references('id')->on('courses')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
+
+        Schema::table('admins', function (Blueprint $table) {
+            // index
+        });
+
+        Schema::table('attendances', function (Blueprint $table) {
+            // column type changes
+            $table->unsignedBigInteger('course_id')->change();
+
+            // foreign keys
+            $table->foreign('user_id')->references('userId')->on('users')
+                ->onDelete('restrict')->onUpdate('cascade');
+
+            $table->foreign('course_id')->references('id')->on('courses')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
+
+        Schema::table('branches', function (Blueprint $table) {
+            // index
+            $table->unique('title');
+        });
+
+        Schema::table('centres', function (Blueprint $table) {
+            $table->unsignedBigInteger('branch_id')->change();
+
+            // index
+            $table->unique('title');
+
+            // foreign keys
+            $table->foreign('branch_id')->references('id')->on('branches')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
+
+        Schema::table('course_sessions', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('course_id')->change();
+
+            // index
+            $table->index(['course_id', 'session']);
+            $table->unique('session');
+
+            // foreign keys
+            $table->foreign('course_id')->references('id')->on('courses')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
+
+        Schema::table('oex_categories', function (Blueprint $table) {
+            // index
+            $table->index('id');
+            $table->unique('name');
+        });
+
+        Schema::table('oex_question_masters', function (Blueprint $table) {
+            $table->renameColumn('exam_id', 'exam_set_id');
+            // $table->unsignedBigInteger('exam_id')->change();
+        });
+
+        Schema::table('oex_question_masters', function (Blueprint $table) {
+            $table->unsignedBigInteger('exam_id')->default(1);
+
+            // foreign keys
+            $table->foreign('exam_id')->references('id')->on('oex_exam_masters')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
 
         Schema::table('oex_results', function (Blueprint $table) {
+            $table->unsignedBigInteger('exam_id')->change();
+            $table->unsignedBigInteger('user_id')->change();
+            $table->integer('yes_ans')->change();
+            $table->integer('no_ans')->change();
+
+
             // index
-            $table->index('exam_id');
-            $table->index('user_id');
-            $table->index(['exam_id','user_id']);
+            $table->index(['exam_id', 'user_id']);
             $table->index('yes_ans');
             $table->index('no_ans');
+            $table->index('created_at');
+            $table->index('id');
+
+
+            // foreign keys
+            $table->foreign('exam_id')->references('id')->on('oex_exam_masters')
+                ->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
+
+
+        Schema::table('programmes', function (Blueprint $table) {
+            $table->unique('title');
+        });
+
+
+
+        Schema::table('user_admission', function (Blueprint $table) {
+            // column type changes
+            $table->unsignedBigInteger('course_id')->change();
+            $table->unsignedBigInteger('session')->change();
+
+
+            // index
+            $table->unique('user_id');
+            $table->index(['user_id', 'course_id']);
+            $table->index('email_sent');
+            $table->index('submitted');
+            $table->index('confirmed');
+
+            // foreign keys
+            $table->foreign('session')->references('id')->on('course_sessions')
+                ->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')
+                ->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('user_id')->references('userId')->on('users')
+                ->onDelete('restrict')->onUpdate('cascade');
+        });
+
+        Schema::table('user_exams', function (Blueprint $table) {
+            // column type changes
+            $table->unsignedBigInteger('user_id')->change();
+            $table->unsignedBigInteger('exam_id')->change();
+
+            // index
+            $table->index('user_id');
+            $table->index('exam_id');
+            $table->index(['user_id', 'exam_id',]);
+            $table->index('exam_joined');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-       
-    }
+    public function down(): void {}
 };
